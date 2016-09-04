bd9759679cd2bc66a93b33f33f320b59ffe53735
David Luecke <daff@neyeon.com>
2016-09-03 18:42:21 -0700

    Get installable version working

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const createServer = require('mysam-server');
+const feathers = require('feathers');
 
 const app = createServer({
   port: 3030,
-  public: path.join(__dirname, '..', '..', 'mysam-frontend'),
+  public: path.join(__dirname, '..', 'node_modules', 'mysam-frontend'),
   plugins: path.join(__dirname, '..', '..', 'plugins'),
   paginate: { default: 100, max: 1000 }
 });
+
+app.use('/node_modules', feathers.static(path.join(__dirname, '..', 'node_modules')));
 
 app.listen(app.get('port')).on('listening', 
   () => console.log(`Listening on ${app.get('port')}`)
