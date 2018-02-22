39f9824633bc45811ace37146d97aa02e97e0d47
David Luecke <daff@neyeon.com>
2018-02-21 20:07:41 -0800

    Add version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const core = require('mysam-core');
 const ui = require('mysam-ui');
+const { version } = require('../package.json');
 
 module.exports = function (el) {
   // Initialize MySam core and its UI
   return ui(el, core());
 };
 
-Object.assign(module.exports, { core, ui });
+Object.assign(module.exports, { core, ui, version });
